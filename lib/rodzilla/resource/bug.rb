a3a2ff7ea0e870cae8e5e7195fc150d9f124b8cc
John Faucett <jwaterfaucett@gmail.com>
2013-10-10 15:10:29 +0200

    Add Bug.create! rpc call

--- lib/rodzilla/resource/bug.rb
+++ lib/rodzilla/resource/bug.rb
@@ -1,6 +1,7 @@
 module Rodzilla
   module Resource
     class Bug < Base
+      REQUIRED_FIELDS = [:product, :component, :summary, :version, :description, :op_sys, :platform, :priority, :severity]
 
       def fields
         rpc_call( rpc_method: "fields" )
@@ -21,6 +22,25 @@ module Rodzilla
       # product, url, id, alias, limit
       def search(query={})
         rpc_call( query.merge(rpc_method: 'search') )
+      end
+
+      # see: http://www.bugzilla.org/docs/tip/en/html/api/Bugzilla/WebService/Bug.html#create
+      # 
+      # Required Fields:
+      # product (string) Required - The name of the product the bug is being filed against.
+      # component (string) Required - The name of a component in the product above.
+      # summary (string) Required - A brief description of the bug being filed.
+      # version (string) Required - A version of the product above; the version the bug was found in.
+      # description (string) Defaulted - The initial description for this bug. Some Bugzilla installations require this to not be blank.
+      # op_sys (string) Defaulted - The operating system the bug was discovered on.
+      # platform (string) Defaulted - What type of hardware the bug was experienced on.
+      # priority (string) Defaulted - What order the bug will be fixed in by the developer, compared to the developer's other bugs.
+      # severity (string) Defaulted - How severe the bug is.
+      # 
+      # Raises an exception if a required field is not provided
+      def create!(params={})
+        REQUIRED_FIELDS.each { |field| raise ArgumentError, "Error: #{field} is required" unless params[field] }
+        rpc_call( params )
       end
 
     end

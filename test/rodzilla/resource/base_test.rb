c4a1bbcf43c32e2f17eebc79d8864e5d7886dd24
John Faucett <jwaterfaucett@gmail.com>
2013-10-14 08:51:57 +0200

    Rewrite tests for ruby 1.9 compat

--- test/rodzilla/resource/base_test.rb
+++ test/rodzilla/resource/base_test.rb
@@ -13,42 +13,74 @@ describe Rodzilla::Resource::Base do
       @base.must_respond_to('base_url=')
     end
 
-    # it { @base.must_respond_to('username') }
-    # it { @base.must_respond_to('username=') }
-    
-    # it { @base.must_respond_to('password') }
-    # it { @base.must_respond_to('password=') }
+    it "has username accessors" do 
+      @base.must_respond_to('username')
+      @base.must_respond_to('username=')
+    end
 
-    # it { @base.must_respond_to('format') }
-    # it { @base.must_respond_to('format=') }
+    it "has password accessors" do 
+      @base.must_respond_to('password')
+      @base.must_respond_to('password=')
+    end
 
-    # it { @base.must_respond_to('request_url') }
-    # it { @base.must_respond_to('request_url=') }
+    it "has format accessors" do 
+      @base.must_respond_to('format')
+      @base.must_respond_to('format=')
+    end
 
-    # it { @base.must_respond_to('response') }
-    # it { @base.must_respond_to('response=') }
+    it "has request_url accessors" do 
+      @base.must_respond_to('request_url')
+      @base.must_respond_to('request_url=')
+    end
 
-    # it { @base.must_respond_to('result') }
-    # it { @base.must_respond_to('result=') }
+    it "has response accessors" do 
+      @base.must_respond_to('response')
+      @base.must_respond_to('response=')
+    end
 
-    # it { @base.must_respond_to('credentials') }
-    # it { @base.must_respond_to('credentials=') }
+    it "has result accessors" do 
+      @base.must_respond_to('result')
+      @base.must_respond_to('result=')
+    end
 
-    # it { @base.must_respond_to('headers') }
-    # it { @base.must_respond_to('headers=') }
+    it "has credentials accessors" do 
+      @base.must_respond_to('credentials')
+      @base.must_respond_to('credentials=')
+    end
 
-    # describe "class methods" do
-    #   it { Rodzilla::Resource::Base.must_respond_to(:demodulize) }
-    # end
+    it "has headers accessors" do 
+      @base.must_respond_to('headers')
+      @base.must_respond_to('headers=')
+    end
 
-    # describe "protected_methods" do
-    #   it { @base.protected_methods.must_include(:prepare_request) }
-    #   it { @base.protected_methods.must_include(:build_url) }
-    #   it { @base.protected_methods.must_include(:build_params) }
-    #   it { @base.protected_methods.must_include(:make_id) }
-    #   it { @base.protected_methods.must_include(:rpc_call) }
-    #   it { @base.protected_methods.must_include(:parse_rpc_response!) }
-    # end
+    describe "class methods" do
+      it "has a demodularize method" do 
+        Rodzilla::Resource::Base.must_respond_to(:demodulize)
+      end
+    end
+
+    describe "protected_methods" do
+      it "has a prepare_request method" do
+        @base.protected_methods.must_include(:prepare_request)
+      end
+      it "has a build_url method" do
+        @base.protected_methods.must_include(:build_url)
+      end
+      it "has a build_params method" do
+        @base.protected_methods.must_include(:build_params)
+      end
+      it "has a make_id method" do
+        @base.protected_methods.must_include(:make_id)
+      end
+
+      it "has a rpc_call method" do
+        @base.protected_methods.must_include(:rpc_call)
+      end
+
+      it "has a parse_rpc_response! method" do
+        @base.protected_methods.must_include(:parse_rpc_response!)
+      end
+    end
 
   end
 end
